f6cdbff94bb3ed2b7372b83a88307246100a9248
Patrick Buzza <pbuzza5@gmail.com>
2018-03-01 11:23:40 -0500

    Added sudoku file with a board,represented as a string of characters, with '.' representing a black cell.

--- Cell.cpp
+++ Cell.cpp
@@ -23,108 +23,105 @@ using namespace std;
 Cell::Cell(int value)
 // overloaded constructor to specify the cell's value
 {
-    this->value = value;
-    this->rowConflicts = vector<bool>(BoardSize);
-    this->colConflicts = vector<bool>(BoardSize);
-    this->conflictValues = vector<bool>(BoardSize);
-    this->squareConflicts = vector<bool>(BoardSize);
+  this->value = value;
+  this->rowConflicts = vector<bool>(BoardSize);
+  this->colConflicts = vector<bool>(BoardSize);
+  this->conflictValues = vector<bool>(BoardSize);
+  this->squareConflicts = vector<bool>(BoardSize);
 }
 
-Cell::Cell() : Cell(Blank) { }
+Cell::Cell() : Cell(Blank) {}
 // Default constructor for cell if no value is passed, initializes cell to be 0
 
 int Cell::getValue() const
 // Returns an cell's value, which is unable to be modified
 {
-    return this->value;
+  return this->value;
 }
 
 ostream &operator<<(ostream &ostr, const Cell &c)
 // overloaded << operator, to print out the value of the cell
 {
-    ostr << c.getValue();
-    return ostr;
+  ostr << c.getValue();
+  return ostr;
 }
 
 void Cell::setValue(int value)
 // Assigns cell's value to value passed in
 {
-    this->value = value;
+  this->value = value;
 }
 
 bool Cell::checkValue(int value)
 // Checks to see if value be assigned to this cell
 {
-    return (!this->conflictValues[value - 1] && this->value == Blank);
+  return (!this->conflictValues[value - 1] && this->value == Blank);
 }
 
 void Cell::modRowConflict(int index, bool addBool)
 // Modifies rowConflicts vector at specified index with either true or false
 // Also adds or removes the value from the conflictValues vector
 {
-    this->rowConflicts[index] = addBool;
-    updateConflicts();
+  this->rowConflicts[index] = addBool;
+  updateConflicts();
 }
 
 void Cell::modColConflict(int index, bool addBool)
 // Modifies colConflicts vector at specified index with either true or false
 // Also adds or removes the value from the conflictValues vector
 {
-    this->colConflicts[index] = addBool;
-    updateConflicts();
+  this->colConflicts[index] = addBool;
+  updateConflicts();
 }
 
 void Cell::modSquareConflict(int index, bool addBool)
 // Modifies squareConflicts vector at specified index with either true or false
 // Also adds or removes the value from the conflictValues vector
 {
-    this->squareConflicts[index] = addBool;
-    updateConflicts();
+  this->squareConflicts[index] = addBool;
+  updateConflicts();
 }
 
 void Cell::updateConflicts()
 // Declares a conflict value if any there is a conflict in the same row, col,
 // or square
 {
-    for(int i = 0; i < BoardSize; i++)
-    {
-        bool isConflict = this->rowConflicts[i] || this->colConflicts[i]
-                          || this->squareConflicts[i];
-        this->conflictValues[i] = isConflict;
-    }
+  for (int i = 0; i < BoardSize; i++) {
+    bool isConflict = this->rowConflicts[i] || this->colConflicts[i]
+        || this->squareConflicts[i];
+    this->conflictValues[i] = isConflict;
+  }
 }
 
 void Cell::printCell()
 // Prints out the cell's value and conflict vector
 {
-    if (this->value == Blank)
-        cout << "|       |";
+  if (this->value == Blank)
+    cout << "|       |";
 
-    else
-        cout << "|   " << this->value << "   |";
+  else
+    cout << "|   " << this->value << "   |";
 
-    cout << "  ";
+  cout << "  ";
 
-    for (int i = 0; i < this->conflictValues.size(); i++)
-    {
-        if (this->conflictValues[i])
-            cout << i + 1 << "  ";
-    }
+  for (int i = 0; i < this->conflictValues.size(); i++) {
+    if (this->conflictValues[i])
+      cout << i + 1 << "  ";
+  }
 }
 
 int Cell::numConstraints()
 // Determines the number of constraints a cell is under and returns that value
 {
-    int numconstraints = 0;
+  int numconstraints = 0;
 
-    for (int i = 0; i < rowConflicts.size(); i++)
-    {
-        if (this->rowConflicts.at(i) == true
-            || this->colConflicts.at(i) == true
-            || this->squareConflicts.at(i) == true)
-            numconstraints++;
-    }
-    return numconstraints;
+  for (int i = 0; i < rowConflicts.size(); i++) {
+    if (this->rowConflicts.at(i)
+        || this->colConflicts.at(i)
+        || this->squareConflicts.at(i))
+      numconstraints++;
+  }
+  return numconstraints;
 }
 
 
--- main.cpp
+++ main.cpp
@@ -7,58 +7,53 @@ using namespace std;
 void solveBoard(string fileName);
 // main function controls which text files to be opened to solve
 // All work is done in the solveBoard function below
-int main()
-{
-    solveBoard("Sudoku1-3.txt");
+int main() {
+  solveBoard("Sudoku1.txt");
 
-    cout << "\nPress ENTER to continue to solve the next set of boards.\n\n";
-    cin.ignore();
+//  cout << "\nPress ENTER to continue to solve the next set of boards.\n\n";
 
-    solveBoard("Sudoku.txt");
-
-    return 0;
+  return 0;
 }
 
 void solveBoard(string fileName)
 // Goes through the text file and generates and solves each sudoku board
 {
-    //opens up text file from passed name
-    ifstream fin;
-    fin.open(fileName);
+  //opens up text file from passed name
+  ifstream fin;
+  fin.open(fileName);
 
-    if (!fin)
-        cout << "File access failure.\n";
+  if (!fin)
+    cout << "File access failure.\n";
 
-    //initializes variables needed to read multiple boards from one file
-    char buffer[BoardSize * BoardSize];
-    char nextCheck = '\n';
-    int numBoards = 0;
-    int recursiveCalls = 0;
+  //initializes variables needed to read multiple boards from one file
+  char buffer[BoardSize * BoardSize];
+  char nextCheck = '\n';
+  int numBoards = 0;
+  int recursiveCalls = 0;
 
-    // Continually reads from the text files in chunks of 81 characters
-    // Ends if there are no more bores
-    while(nextCheck == '\n')
-    {
-        fin.read(buffer, sizeof(buffer));
-        Board b = Board(buffer);
-        b.print();
-        b.solve();
-        b.print();
-        cout << "Solving this board took " << b.getRecursiveCalls();
-        cout <<" recursive calls. \n\n";
-        recursiveCalls += b.getRecursiveCalls();
-        numBoards++;
-        fin >> noskipws >> nextCheck;
-    }
-    //determines average number of calls by adding all recursive calls made
-    // for the text file, and dividing by the number of boards solved
-    int averageCalls = recursiveCalls / numBoards;
+  // Continually reads from the text files in chunks of 81 characters
+  // Ends if there are no more bores
+  while (nextCheck == '\n') {
+    fin.read(buffer, sizeof(buffer));
+    Board b = Board(buffer);
+    b.print();
+    b.solve();
+    b.print();
+    cout << "Solving this board took " << b.getRecursiveCalls();
+    cout << " recursive calls. \n\n";
+    recursiveCalls += b.getRecursiveCalls();
+    numBoards++;
+    fin >> noskipws >> nextCheck;
+  }
+  //determines average number of calls by adding all recursive calls made
+  // for the text file, and dividing by the number of boards solved
+  int averageCalls = recursiveCalls / numBoards;
 
-    //prints out some interesting info...
-    cout << "===============================================================";
-    cout << endl << endl;
-    cout << "Solving " << numBoards << " boards required ";
-    cout << recursiveCalls;
-    cout << " recursive calls, an average of ";
-    cout << averageCalls << ".\n\n";
+  //prints out some interesting info...
+  cout << "===============================================================";
+  cout << endl << endl;
+  cout << "Solving " << numBoards << " boards required ";
+  cout << recursiveCalls;
+  cout << " recursive calls, an average of ";
+  cout << averageCalls << ".\n\n";
 }
